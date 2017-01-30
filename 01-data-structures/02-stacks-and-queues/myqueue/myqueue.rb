f8e43f0ee5644859755f7b287a2b67e21bbc4023
emikod@mac.com <emikod@mac.com>
2017-01-29 17:34:12 -0700

    Completes MyQueue class

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -1,18 +1,29 @@
 class MyQueue
-  attr_accessor :head
-  attr_accessor :tail
+  attr_accessor :queue
 
   def initialize
-    @queue = Array.new
-    @head = @queue[0]
+    self.queue = Array.new
+    # @head = @queue[0]
+    # @tail = @queue[-1]
+  end
+
+  def head
+    queue[0]
+  end
+
+  def tail
+    queue[-1]
   end
 
   def enqueue(element)
+    queue.insert(0, element)
   end
 
   def dequeue
+    queue.delete_at(-1)
   end
 
   def empty?
+    (queue.length == 0) ? true : false
   end
 end
--- 01-data-structures/02-stacks-and-queues/myqueue/spec/myqueue_spec.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/spec/myqueue_spec.rb
@@ -1,6 +1,6 @@
 include RSpec
 
-require_relative 'myqueue'
+require_relative '../myqueue'
 
 RSpec.describe MyQueue, type: Class do
   let(:q) { MyQueue.new }
