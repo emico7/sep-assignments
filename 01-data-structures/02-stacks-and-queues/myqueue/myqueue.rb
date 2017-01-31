f959285625460a2894829e586bd4fb6f91c7ba05
emikod@mac.com <emikod@mac.com>
2017-01-31 16:04:38 -0700

    Adds update head and tail function

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -1,29 +1,38 @@
 class MyQueue
-  attr_accessor :queue
+  attr_accessor :queue, :head, :tail
 
   def initialize
-    self.queue = Array.new
-    # @head = @queue[0]
-    # @tail = @queue[-1]
-  end
-
-  def head
-    queue[0]
-  end
-
-  def tail
-    queue[-1]
+    @queue = Array.new
+    @head
+    @tail
   end
 
   def enqueue(element)
     queue.insert(0, element)
+    update_head_and_tail
   end
 
   def dequeue
     queue.delete_at(-1)
+    update_head_and_tail
   end
 
   def empty?
     (queue.length == 0) ? true : false
   end
+
+  private
+
+  def update_head_and_tail
+    @head = queue[0]
+    @tail = queue[-1]
+  end
+
+  # def head
+  #   queue[0]
+  # end
+  #
+  # def tail
+  #   queue[-1]
+  # end
 end
