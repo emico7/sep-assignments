9c251498d940b3be8b7178e55c9f1ebf8d7bc945
emikod@mac.com <emikod@mac.com>
2017-02-07 13:57:37 -0700

    Passes all the tests

--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -11,41 +11,33 @@ class LinkedList
 
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
-    if @head == nil && @tail == nil
-      @head = node
-      @tail = node
-    else
-      current_tail = @tail
-      @tail = node
-      current_tail.next = @tail
-    end
+    @head ||= node
+
+    @tail.next = node if @tail
+    @tail = node
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
-    if @head == @tail
-      @head = nil
-      @tail = nil
-    elsif @tail == nil
-      return nil
-    else
-      node_being_chkd = @head.next
-      while node_being_chkd != @tail
+    if @head
+      node_being_chkd = @head
+      while node_being_chkd && (node_being_chkd.next != @tail)
         node_being_chkd = node_being_chkd.next
       end
       @tail = node_being_chkd
-      @tail.next = nil
+      @tail.next = nil if @tail
+    else
+      nil
     end
   end
 
   # This method prints out a representation of the list.
   def print
     arr = []
-    while @tail
-      arr.unshift(@tail.data)
-      @tail = @tail.next
+    while @head
+      arr << @head.data
+      @head = @head.next
     end
-    # arr.each { |x| puts x }
     puts arr
   end
 
@@ -62,42 +54,29 @@ class LinkedList
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
-    if @head
-      current_head = @head
-      @head = node
-      current_head.next = @head
-    elsif @head == nil
-      @head = node
-      @tail = node
-    end
+    @tail ||= node
+    node.next = @head if @head
+    @head = node
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
-    return nil if @head == nil
-
-    if @tail == @head
-      @head = nil
-      @tail = nil
-    else
-      node_being_chkd = @tail
-      while node_being_chkd.next != @head
-        node_being_chkd = node_being_chkd.next
-      end
-      @head = node_being_chkd
-      @head.next = nil
-    end
+    @head = @head.next if @head
   end
 
   private
 
   def remove_node(node)
-    target_node = @tail.next
-    while target_node != node
-      one_before_target = target_node
-      target_node = target_node.next
-      one_after_target = target_node.next
+    target_node = @head.next
+    if target_node == node
+      @head.next = @tail
+    else
+      while target_node != node
+        one_before_target = target_node
+        target_node = target_node.next
+        one_after_target = target_node.next
+      end
+      one_before_target.next = one_after_target
     end
-    one_after_target.next = one_before_target
   end
 end
